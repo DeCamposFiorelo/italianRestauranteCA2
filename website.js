62546a3a376ce395df470512810882139e988ce0
DeCamposFiorelo <suelendecamposfiorelo@gmail.com>
2020-04-24 23:20:20 +0000

    install and conf of method-override

--- website.js
+++ website.js
@@ -5,10 +5,11 @@ const express = require('express');
 const app = express();
 const expressLayouts = require('express-ejs-layouts');
 const bodyParser = require('body-parser')
+const methodOverride = require('method-override')
 const indexRouter = require('./router/index')
 const pizzaRouter = require('./router/pizzas')
 const sideRouter = require('./router/sides')
-const methodOverride = require('method-override')
+
 //connect to the database(Im using env to hide the credentials)- Mikhail showed in class
 
 const mongoose = require('mongoose')
@@ -22,12 +23,13 @@ app.set('view engine','ejs')//Im using the ejs for view engine
 app.set('views',__dirname +'/views')//where my views is comming from
 app.set('layout','layouts/layout')//every file it will be put inside the layout
 app.use(expressLayouts)
+app.use(methodOverride('_method'))
 app.use(express.static('public'))//files,javascript,image
 app.use(bodyParser.urlencoded({limit:'10mb', extended:false}))
 app.use('/',indexRouter)// index page
 app.use('/pizzas',pizzaRouter)
 app.use('/sides',sideRouter)
-app.use(methodOverride('_method'))
+
 
 const Port = process.env.Port || 3000;// port to use
 
